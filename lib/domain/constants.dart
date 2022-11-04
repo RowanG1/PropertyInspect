4e4be631944a19ab3f32d3e760b441cbc585280e
Rowan Gontier <rowan.gontier@bilue.com.au>
2022-11-04 16:49:13 +1100

    Update email.

--- lib/domain/constants.dart
+++ lib/domain/constants.dart
@@ -19,7 +19,7 @@ class Constants {
   static final largePadding = 32.toDouble();
 
 // Text strings
-  static const contactEmail= "rgontier@gmail.com";
+  static const contactEmail= "info@property-checkin.com.au";
   static const visitorRegistrationHeading = 'Please complete your registration.';
   static const listerRegistrationHeading = 'Please complete your registration'
       '.';
