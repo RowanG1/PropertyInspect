4a325c2583d13e39285d29df10c611682ae95a6c
Rowan Gontier <rowan.gontier@bilue.com.au>
2022-11-14 11:02:55 +1100

    Checkbox error colors working.

--- lib/ui/widgets/checkbox_form_field.dart
+++ lib/ui/widgets/checkbox_form_field.dart
@@ -2,6 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CheckboxFormField extends FormField<bool> {
+  static Color getFillColor(Set<MaterialState> states) {
+    return Colors.grey;
+  }
+
+  static Color getFillColorError(Set<MaterialState> states) {
+    return Colors.red;
+  }
+
+  static MaterialStateColor getFillMaterialStateColor(String? error) {
+    if (error != null) {
+      return MaterialStateColor.resolveWith(getFillColorError);
+    } else {
+      return MaterialStateColor.resolveWith(getFillColor);
+    }
+  }
+
   CheckboxFormField(
       {FormFieldSetter<bool>? onSaved, required FormFieldValidator<bool> validator, bool initialValue = false, bool autovalidate = false})
       : super(
@@ -11,7 +27,8 @@ class CheckboxFormField extends FormField<bool> {
             builder: (FormFieldState<bool> state) {
               return Checkbox(
                   value: state.value,
-                  activeColor: Colors.green,
+                  activeColor: !state.hasError ? Colors.green : Colors.red,
+                  fillColor: getFillMaterialStateColor(state.errorText),
                   onChanged: (newValue) {
                     state.didChange(newValue ?? false);
                   });
